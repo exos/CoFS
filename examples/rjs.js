d5b84e6c2a22ad1fe5067a7558a565304485e208
exos <tioscar@gmail.com>
2015-01-23 02:54:19 -0300

    Update examples

---
+++ examples/rjs.js
@@ -0,0 +1,54 @@
+
+requirejs.config({
+    baseUrl: '../',
+    paths: {
+        cofs: 'src',
+        buffer: 'bower_components/buffer/buffer',
+        async: 'bower_components/async/lib/async',
+        "events-emitter": 'bower_components/events-emitter/src'
+    }
+});
+
+require(['cofs/fs'], function (CoFS) {
+
+    var $file = document.getElementById('ifile');
+    var $cmdRead = document.getElementById('cmd-read');
+    var $cmdSave = document.getElementById('cmd-save');
+    var $filename = document.getElementById('filename');
+    var $txt = document.getElementById('text');
+
+    var fs = new CoFS();
+
+    $cmdRead.onclick = function (ev) {
+        var files = $file.files;
+
+        if (!files || !files.length) {
+            return alert("No file selected");
+        }
+
+        fs.readFile(files[0], function (err, data) {
+            if (err) {
+                return alert ("Error! " + err.toString());
+            }
+
+            $txt.value = data.toString('utf-8');
+
+        });
+
+    };
+
+    $cmdSave.onclick = function (ev) {
+
+        var fsWriteable = new CoFS({
+            fs: window.TEMPORARY     
+        });
+        
+        fsWriteable.writeFile($filename.value, $txt.value, 'utf-8', function (err) {
+            if (err) {
+                return alert("Error! " + err.toString());     
+            }
+        });
+
+    };
+
+});
